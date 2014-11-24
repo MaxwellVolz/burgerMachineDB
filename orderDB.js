e0cb0c3cca91892a584640f29d9ba01f72060834
maxwellvolz <maxwellvolz@gmail.com>
2014-11-24 11:33:41 -0800

    update to newer db

---
+++ orderDB.js
@@ -0,0 +1,87 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+var a1title = "Double cheeseburger\nand 2 large\n fries please.\nthanks a ton\nyeah rip\nripripriprip";
+var a2title = "Double large\nfry.";
+var a3title = "Double medium\nfry";
+var a4title = "Double small\n fry";
+var a5title = "Double large\nsodas";
+var a6title = "That's\nall for\nnow!";
+	
+var a1order = [
+    2,//lgFriesMade
+    0,//mdFriesMade
+    0,//smFriesMade
+    2,//pattiesMade
+    2,//cheeseMade
+    0,//picklesMade
+	0,//tomatoesMade
+	0,//onionsMade
+	0,//sauceMade
+	0,//lgDrinksMade
+	0,//mdDrinksMade
+	0//smDrinksMade
+	];
+var a2order = [
+    2,//lgFriesMade
+    0,//mdFriesMade
+    0,//smFriesMade
+    0,//pattiesMade
+    0,//cheeseMade
+    0,//picklesMade
+	0,//tomatoesMade
+	0,//onionsMade
+	0,//sauceMade
+	0,//lgDrinksMade
+	0,//mdDrinksMade
+	0//smDrinksMade
+	];
+var a3order = [
+    0,//lgFriesMade
+    2,//mdFriesMade
+    0,//smFriesMade
+    0,//pattiesMade
+    0,//cheeseMade
+    0,//picklesMade
+	0,//tomatoesMade
+	0,//onionsMade
+	0,//sauceMade
+	0,//lgDrinksMade
+	0,//mdDrinksMade
+	0//smDrinksMade
+	];
+var a4order = [
+    0,//lgFriesMade
+    0,//mdFriesMade
+    2,//smFriesMade
+    0,//pattiesMade
+    0,//cheeseMade
+    0,//picklesMade
+	0,//tomatoesMade
+	0,//onionsMade
+	0,//sauceMade
+	0,//lgDrinksMade
+	0,//mdDrinksMade
+	0//smDrinksMade
+	];
+var a5order = [
+    0,//lgFriesMade
+    0,//mdFriesMade
+    0,//smFriesMade
+    0,//pattiesMade
+    0,//cheeseMade
+    0,//picklesMade
+	0,//tomatoesMade
+	0,//onionsMade
+	0,//sauceMade
+	2,//lgDrinksMade
+	0,//mdDrinksMade
+	0//smDrinksMade
+	];
