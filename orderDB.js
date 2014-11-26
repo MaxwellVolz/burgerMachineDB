17c053fb760760d38710d778fb907d30189283c9
maxwellvolz <maxwellvolz@gmail.com>
2014-11-26 01:04:52 -0800

    added c, get money

--- orderDB.js
+++ orderDB.js
@@ -19,24 +19,26 @@ function Update () {
 //create a line-break with \n no spaces needed
 //6 lines maximum - short orders can start with a \n - needs testing.
 
-var a1title = "Hey.\nCan you make me\n1 Large Fry\nand\n1 Cheeseburger?";
-var a2title = "Could you do\n1 Small Fry and\n2 Small Drinks?";
-var a3title = "All right!\n1 Burger\nwith Tomato\nand 1 Large Fry.";
-var a4title = "Burger Machine?\n2* Medium Drinks.\nPlease.";
-var a5title = "Hi!\n1 Cheeseburger with\nEverything on it!";
-var a6title = "Can I have\n1 Double Burger\nwith Pickles\nand 2*\nMedium Fries.";
-var a7title = "Hello?\nYeah.\n1\nTriple Cheeseburger.\nThanks.";
+var a1title = "Hey.\nCan you make me\n1 large fry\nand\n1 cheeseburger?";
+var a2title = "\nCould you do\n1 small fry and\n2 small drinks?";
+var a3title = "All right!\n1 burger\nwith tomato\nand 1 large fry.";
+var a4title = "\nBurger Machine?\n2 medium drinks.\nPlease.";
+var a5title = "\nHi!\n1 cheeseburger with\neverything on it!";
+var a6title = "Can I have\n1 double burger\nwith pickles\nand 2\nmedium fries.";
+var a7title = "Hello?\nYeah.\n1\nTriple cheeseburger.\nThanks.";
 
-var b1title = "\nHey.\nCan you make me\n3 Large Fry";
-var b2title = "Could you do\n1 Small Fry and\n2 Small Drinks?";
-var b3title = "All right!\n1 Burger\nwith Tomato\nand 1 Large Fry.";
-var b4title = "Burger Machine?\n2* Medium Drinks.\nPlease.";
-var b5title = "Hi!\n1 Cheeseburger with\nEverything on it!";
-var b6title = "Can I have\n1 Double Burger\nwith Pickles\nand 2*\nMedium Fries.";
-var b7title = "Hello?\nYeah.\n1\nTriple Cheeseburger.\nThanks.";
+var b1title = "\nHey.\nCan you make me\n3 large fries";
+var b2title = "Could you do\n2 small fry and\n1 small drink?";
+var b3title = "All right!\n1 burger\nwith tomato\nand 1 medium fry.";
+var b4title = "\nBurger Machine?\n2 medium drinks.\nPlease.";
+var b5title = "\nHi!\n1 Cheeseburger with\neverything on it!";
+var b6title = "Can I have\n1 double burger\nwith 2 pickles\nand 1\nmedium fries.";
+var b7title = "Hello?\nYeah.\n2\nTriple cheeseburgers.\nThanks.";
+
+var c1title = "Hey.\nCan you make me\n1 medium fry\nand\n1 cheeseburger?";
 	
 var a1order = [
-    3,//lgFriesMade
+    1,//lgFriesMade
     0,//mdFriesMade
     0,//smFriesMade
     1,//pattiesMade
@@ -151,7 +153,7 @@ var b1order = [
 var b2order = [
     0,//lgFriesMade
     0,//mdFriesMade
-    1,//smFriesMade
+    2,//smFriesMade
     0,//pattiesMade
     0,//cheeseMade
     0,//picklesMade
@@ -160,11 +162,11 @@ var b2order = [
 	0,//sauceMade
 	0,//lgDrinksMade
 	0,//mdDrinksMade
-	2//smDrinksMade
+	1//smDrinksMade
 	];
 var b3order = [
-    1,//lgFriesMade
-    0,//mdFriesMade
+    0,//lgFriesMade
+    1,//mdFriesMade
     0,//smFriesMade
     1,//pattiesMade
     0,//cheeseMade
@@ -206,11 +208,11 @@ var b5order = [
 	];
 var b6order = [
     0,//lgFriesMade
-    2,//mdFriesMade
+    1,//mdFriesMade
     0,//smFriesMade
     2,//pattiesMade
     0,//cheeseMade
-    1,//picklesMade
+    2,//picklesMade
 	0,//tomatoesMade
 	0,//onionsMade
 	0,//sauceMade
@@ -222,8 +224,22 @@ var b7order = [
     0,//lgFriesMade
     0,//mdFriesMade
     0,//smFriesMade
-    3,//pattiesMade
-    3,//cheeseMade
+    6,//pattiesMade
+    6,//cheeseMade
+    0,//picklesMade
+	0,//tomatoesMade
+	0,//onionsMade
+	0,//sauceMade
+	0,//lgDrinksMade
+	0,//mdDrinksMade
+	0//smDrinksMade
+	];
+var c1order = [
+    0,//lgFriesMade
+    1,//mdFriesMade
+    0,//smFriesMade
+    1,//pattiesMade
+    1,//cheeseMade
     0,//picklesMade
 	0,//tomatoesMade
 	0,//onionsMade
